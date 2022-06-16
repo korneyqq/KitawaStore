fcdd823713cf63f3bc141a5ee355d93521da3bb8
kitawanv <korney.novikov7.kn@gmail.com>
2022-06-16 12:01:55 +0300

    creating database and templates

---
+++ store/products/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from products.models import Product, ProductCategory
+
+
+def index(request):
+    context = {
+        'title': 'Store'
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - catalog',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
